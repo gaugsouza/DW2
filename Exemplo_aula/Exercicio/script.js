cb4d2c7791750b49fd97a642107e4fcd7f7ee5d1
gaugsouza <guilhermeguis@outlook.com>
2019-10-07 20:35:58 -0300

    07/10 e exercicio finalizado

--- Exemplo_aula/Exercicio/script.js
+++ Exemplo_aula/Exercicio/script.js
@@ -1,18 +1,28 @@
 $(function() {
     let resultado = 0;
+    let produtos = [];
     $.getJSON('../jQuery/dadosExemplo.json', function(p) {
         $('#stage').append('<tr class="tableHeader"><td> Produto </td><td> Valor </td></tr>');             
         p.produtos.forEach(produto => {
             $('#stage').append('<tr><td>' + produto.nome + '</td><td>' + produto.valor + '</td></tr>');    
             resultado += parseFloat(produto.valor);
         });
+        $('#stage > tr').toArray().forEach(produto => {
+            produtos.push(produto);
+        });
         $('#stage').append('<tr><td> Total: </td><td>' + resultado + '</td></tr>');    
     });
     $("form").submit(function(e){
         e.preventDefault();
         let campos = $('form').serializeArray();
+        $('#stage').html("");
+        produtos.forEach(produto => {
+            $('#stage').append(produto);
+        })
         $('#stage').append('<tr><td>'+ campos[0].value +'</td><td>' + parseFloat(campos[1].value).toFixed(2) + '</td></tr>');
-        resultado += parseFloat(campos.valor);
+        produtos.push($('#stage > tr').last());
+        console.log(campos);
+        resultado += parseFloat(campos[1].value);
+        $('#stage').append('<tr><td> Total: </td><td>' + resultado + '</td></tr>'); 
     })
 });
-
